c36a548d8afc0b75ddb112c7abb66cf1b47661f8
“vivek-infineon” <Sisodiya.external@infineon.com>
2022-11-25 13:08:47 +0530

    Updated SocialFeatures sql
    Signed-off-by: “vivek-infineon” <“Sisodiya.external@infineon.com”>

--- databasesDumps/SocialFeaturesver1.0 b1.sql
+++ databasesDumps/SocialFeaturesver1.0 b1.sql
@@ -1,1 +1,39 @@
-CREATE DATABASE `onlinedocumentation-social` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+CREATE DATABASE `onlinedocumentation-social` 
+/*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ 
+/*!80016 DEFAULT ENCRYPTION='N' */;
+USE `onlinedocumentation-social` ;
+CREATE TABLE `messages` (
+  `messageid` int NOT NULL AUTO_INCREMENT,
+  `messagetext` varchar(45) NOT NULL,
+  `messagedate` datetime DEFAULT NULL,
+  PRIMARY KEY (`messageid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `page` (
+  `pageid` int NOT NULL AUTO_INCREMENT,
+  `pageurl` varchar(45) DEFAULT NULL,
+  `pagecol` varchar(45) NOT NULL,
+  PRIMARY KEY (`pageid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `user` (
+  `userid` int NOT NULL,
+  `useremail` varchar(45) NOT NULL,
+  `username` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='master';
+
+CREATE TABLE `page-message-user` (
+  `page-message-userid` int NOT NULL,
+  `pageid` int NOT NULL,
+  `messgeid` int NOT NULL,
+  `userid` int NOT NULL,
+  PRIMARY KEY (`page-message-userid`),
+  UNIQUE KEY `page-message-user_id_UNIQUE` (`page-message-userid`),
+  KEY `fkpage_idx` (`pageid`),
+  KEY `fkuser_idx` (`userid`),
+  KEY `fkmessage_idx` (`messgeid`),
+  CONSTRAINT `fkmessage` FOREIGN KEY (`messgeid`) REFERENCES `messages` (`messageid`),
+  CONSTRAINT `fkpage` FOREIGN KEY (`pageid`) REFERENCES `page` (`pageid`),
+  CONSTRAINT `fkuser` FOREIGN KEY (`userid`) REFERENCES `user` (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
